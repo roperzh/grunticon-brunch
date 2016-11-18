413d4e3620dc5f8785dc538899a23801c40f5f8e
Roberto Dip <dip.jesusr@gmail.com>
2016-11-18 18:27:37 -0300

    Initial package structure

---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+class GrunticonCompiler {
+  brunchPlugin: true
+
+  onCompile({allSourceFiles, sourceFiles, type}) {
+  }
+}
+
+module.exports GrunticonCompiler;
---
+++ test.js
@@ -0,0 +1,3 @@
+function assert(condition, message) {
+    if (!condition) throw new Error(message)
+}
